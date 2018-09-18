e239492f23aea967e1a28fe1669d72c876ec4b55
Shruthi <acharyashruthi1608@gmail.com>
2018-09-18 12:08:09 +0530

    version2

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+
+    int sum=0,n,i;
+    printf("enter n:");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+
+    printf("SUM:%d",sum);
+}
